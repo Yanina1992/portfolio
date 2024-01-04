2d544aa4008931f8dd649faec2319309f794d5f1
Yanina Agüero <yaninaaguero@hotmail.it>
2024-01-04 19:07:23 +0100

    Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PresentazioneComponent } from './presentazione/presentazione.component';
+import { ProgettiComponent } from './progetti/progetti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PresentazioneComponent,
+    ProgettiComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/presentazione/presentazione.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentazione',
+  templateUrl: './presentazione.component.html',
+  styleUrls: ['./presentazione.component.scss']
+})
+export class PresentazioneComponent {
+
+}
